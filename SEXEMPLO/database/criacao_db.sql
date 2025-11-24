e6df6c20be85b51e68f72ba8808b2b27edf7890f
Daniel de Jesus <Danieljesus280407@gmail.com>
2025-11-24 08:54:43 -0300

    Update criacao_db.sql

--- SEXEMPLO/database/criacao_db.sql
+++ SEXEMPLO/database/criacao_db.sql
@@ -1,13 +1,9 @@
--- Instale o Banco de Dados e rode esse comando para criar o banco de dados da sua aplicaçao
-
-DROP database mydatabase;
-CREATE database mydatabase;
-USE mydatabase;
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    login VARCHAR(255) NOT NULL,
-    perfil VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+create database hiddenroute;
+use hiddenroute;
+create table empresa (
+  id int auto_increment primary key,
+  nome_empresa varchar(100) not null,
+  bairro varchar(100) not null,
+  cnpj varchar(14) not null,
+  avenida varchar(100) not null
 );
