619d219eb1112d6854ecaa5b74fa9c5820b553f1
Daniel de Jesus <Danieljesus280407@gmail.com>
2025-12-01 10:35:18 -0300

    Delete SEXEMPLO directory

--- SEXEMPLO/SEXEMPLO-WEB/README.md
+++
@@ -1,3 +0,0 @@
- npm create vite@latest frontend
- npm install 
- npm run dev
--- SEXEMPLO/database/criacao_db.sql
+++
@@ -1,9 +0,0 @@
-create database hiddenroute;
-use hiddenroute;
-create table empresa (
-  id int auto_increment primary key,
-  nome_empresa varchar(100) not null,
-  bairro varchar(100) not null,
-  cnpj varchar(14) not null,
-  avenida varchar(100) not null
-);
